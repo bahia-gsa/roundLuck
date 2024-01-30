205fab41222ee52b42ad5c380776e59ccbeaabd8
bahia.gsa <bahia.gsa@gmail.com>
2024-01-30 21:40:59 +0100

    added animation

--- angular/src/app/pages/index/index.component.ts
+++ angular/src/app/pages/index/index.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {QuarkusService} from "../../services/quarkus.service";
 import {CookieService} from "ngx-cookie-service";
 import {UserLogged} from "../../model/UserLogged";
 import {AuthService} from "../../services/auth.service";
+import { gsap } from 'gsap';
 
 
 @Component({
@@ -10,9 +11,19 @@ import {AuthService} from "../../services/auth.service";
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
 
   userLogged!: UserLogged;
+  phrases: string[] = [
+    "create a a game",
+    "easily add, remove, and edit players",
+    "randomly select a player",
+    "keep track of who's turn it is"
+  ];
+  currentPhraseIndex: number = 0;
+  currentLetterIndex: number = 0;
+  currentPhrase: string = '';
+
 
   constructor(private quarkusService: QuarkusService,
               private cookieService: CookieService) {
@@ -21,6 +32,47 @@ export class IndexComponent {
       const jsonObject = JSON.parse(decodeURIComponent(encodedCookieValue));
       this.userLogged = {token: jsonObject.token, email: jsonObject.email};
       this.getQuarkusAuthenticationToken(this.userLogged)
+    }
+  }
+
+  ngOnInit(): void {
+    this.animateTitle();
+    this.displayNextPhrase();
+
+  }
+
+  animateTitle(): void {
+    const tl = gsap.timeline({ repeat: -1 });
+    const animationSettings = {
+      duration: 30,
+      backgroundPosition: "-960px 0",
+    };
+    tl.to(".letter", animationSettings);
+  }
+
+  displayNextPhrase() {
+    if (this.currentPhraseIndex < this.phrases.length) {
+      this.currentPhrase = this.phrases[this.currentPhraseIndex];
+      this.currentLetterIndex = 0;
+      this.typeNextLetter();
+    }
+  }
+
+  typeNextLetter() {
+    if (this.currentLetterIndex < this.currentPhrase.length) {
+      setTimeout(() => {
+        this.currentLetterIndex++;
+        this.typeNextLetter();
+      }, 100);
+    } else {
+      if (this.currentPhraseIndex < this.phrases.length - 1) {
+        this.currentPhraseIndex++;
+      } else {
+        this.currentPhraseIndex = 0;
+      }
+      setTimeout(() => {
+        this.displayNextPhrase();
+      }, 1000);
     }
   }
 
